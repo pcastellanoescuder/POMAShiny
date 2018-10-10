e175485e4d92ed25caa283d89055ecca71523f74
pcastellanoescuder <polcaes@gmail.com>
2018-10-10 16:22:53 +0200

    Installation guide

---
+++ instructions/install_packages_POMA.R
@@ -0,0 +1,27 @@
+installifnot <- function(pckgName){
+  if (!(require(pckgName, character.only = TRUE))) {
+    install.packages(pckgName, dep = TRUE)
+    require(pckgName, character.only = TRUE)
+  }
+}
+
+pk1 <- c("shiny", "shinydashboard", "DT", "reshape2", "ggplot2", "gplots", "scales", "plotly", "readxl", "glmnet", "ggvis", "shinyhelper",
+         "broom", "readr", "markdown", "ggthemes", "dplyr", "ggrepel", "ggfortify", "shinyBS", "glue", "limma", "tidyr", "mixOmics", "devtools",
+         "Rcpp")
+
+for (i in 1:length(pk1)){
+  installifnot(pk1[i])
+}
+
+installBiocifnot <- function(pckgName){
+  if (!(require(pckgName, character.only = TRUE))) {
+    source("http://Bioconductor.org/biocLite.R")
+    biocLite(pckgName)
+    require(pckgName, character.only = TRUE)
+  }
+}
+
+installBiocifnot("impute")
+
+devtools::install_github("vqv/ggbiplot")
+devtools::install_github("nik01010/dashboardthemes")
