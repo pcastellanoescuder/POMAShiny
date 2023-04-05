c5e01de45879f62a0793686a340ab59852df2336
pcastellanoescuder <polcaes@gmail.com>
2023-04-05 13:45:46 -0400

    fix UI

--- app/ui.R
+++ app/ui.R
@@ -19,184 +19,203 @@
 source("helpers.R")
 source("themes.R")
 
-bs4DashPage(
-  old_school = FALSE,
-  sidebar_min = TRUE,
-  sidebar_collapsed = FALSE,
-  controlbar_collapsed = TRUE,
-  controlbar_overlay = TRUE,
-  title = "POMAShiny",
+dashboardPage(
+  # old_school = FALSE,
+  # sidebar_min = TRUE,
+  # sidebar_collapsed = FALSE,
+  # controlbar_collapsed = TRUE,
+  # controlbar_overlay = TRUE,
+  # title = "POMAShiny",
+  
+  header = dashboardHeader(
+    title = span(img(src = "pomalogo.png", height = 35), "POMA"),
+    titleWidth = 300,
+    dropdownMenu(
+      type = "notifications",
+      headerText = strong("HELP"),
+      icon = icon("question"),
+      badgeStatus = NULL
+    ),
+    tags$li(
+      a(
+        strong("ABOUT POMA"),
+        height = 40,
+        href = "xxx",
+        title = "",
+        target = "_blank"
+      ),
+      class = "dropdown"
+    )
+  ),
   
   ## NAVBAR ----------------------------------------------------------------------
   
-  bs4DashNavbar(
-    skin = "dark",
-    status = "primary",
-    border = TRUE,
-    sidebarIcon = "chevron-left",
-    controlbarIcon = "th",
-    fixed = FALSE,
-    HTML('<script async defer src="https://buttons.github.io/buttons.js"></script>
-         <a class="github-button" href="https://github.com/pcastellanoescuder/POMAShiny" 
-         data-show-count="true" aria-label="Star pcastellanoescuder/POMAShiny on GitHub">Star</a>'),
-    HTML('<script async defer src="https://buttons.github.io/buttons.js"></script>
-         <a class="github-button" href="https://github.com/pcastellanoescuder/POMAShiny/issues" 
-         data-show-count="true" aria-label="Issue pcastellanoescuder/POMAShiny on GitHub">Issue</a>'),
-    includeScript("google-analytics.js")
-    ),
+  # bs4DashNavbar(
+  #   skin = "dark",
+  #   status = "primary",
+  #   border = TRUE,
+  #   sidebarIcon = "chevron-left",
+  #   controlbarIcon = "th",
+  #   fixed = FALSE,
+  #   HTML('<script async defer src="https://buttons.github.io/buttons.js"></script>
+  #        <a class="github-button" href="https://github.com/pcastellanoescuder/POMAShiny"
+  #        data-show-count="true" aria-label="Star pcastellanoescuder/POMAShiny on GitHub">Star</a>'),
+  #   HTML('<script async defer src="https://buttons.github.io/buttons.js"></script>
+  #        <a class="github-button" href="https://github.com/pcastellanoescuder/POMAShiny/issues"
+  #        data-show-count="true" aria-label="Issue pcastellanoescuder/POMAShiny on GitHub">Issue</a>'),
+  #   includeScript("google-analytics.js")
+  #   ),
   
   ## SIDEBAR ----------------------------------------------------------------------
   
-  bs4DashSidebar(
-    skin = "dark",
-    status = "warning",
-    title = HTML("<b>POMAShiny</b>"),
-    brandColor = "warning",
-    url = "https://github.com/pcastellanoescuder/POMAShiny",
-    src = "https://github.com/pcastellanoescuder/POMA/blob/master/man/figures/logo.png?raw=true",
-    elevation = 3,
-    opacity = 0.8,
+  sidebar = dashboardSidebar(
+    # skin = "dark",
+    # status = "warning",
+    # title = HTML("<b>POMAShiny</b>"),
+    # brandColor = "warning",
+    # url = "https://github.com/pcastellanoescuder/POMAShiny",
+    # src = "https://github.com/pcastellanoescuder/POMA/blob/master/man/figures/logo.png?raw=true",
+    # elevation = 3,
+    # opacity = 0.8,
       
-    bs4SidebarMenu(
+    sidebarMenu(
       
-      bs4SidebarMenuItem("Home", tabName = "home", icon = "home"),
-      bs4SidebarMenuItem("Upload Data", tabName = "inputdata", icon = "upload"),
-      bs4SidebarMenuItem("Pre-processing", tabName = "preprocessing", icon = "wrench", startExpanded = FALSE,
-                         bs4SidebarMenuSubItem("Impute Values", tabName = "impute_vals", icon = "angle-double-right"),
-                         bs4SidebarMenuSubItem("Normalization", tabName = "normalization", icon = "angle-double-right"),
-                         bs4SidebarMenuSubItem("Outlier Detection", tabName = "outliers", icon = "angle-double-right")
-                         ),
-      bs4SidebarMenuItem("EDA", tabName = "visualization", icon = "search", startExpanded = FALSE,
-                         bs4SidebarMenuSubItem("Volcano Plot", tabName = "volcanoPlot", icon = "angle-double-right"),
-                         bs4SidebarMenuSubItem("Boxplot", tabName = "boxPlot", icon = "angle-double-right"),
-                         bs4SidebarMenuSubItem("Density Plot", tabName = "density", icon = "angle-double-right"),
-                         bs4SidebarMenuSubItem("Heatmap", tabName = "HeatMap", icon = "angle-double-right")
-                         ),
-      bs4SidebarMenuItem("Statistical Analysis", tabName = "statistics", icon = "chart-bar", startExpanded = FALSE,
-                         bs4SidebarMenuSubItem("Univariate Analysis", tabName = "univariate", icon = "angle-double-right"),
-                         bs4SidebarMenuSubItem("Multivariate Analysis", tabName = "multivariate", icon = "angle-double-right"),
-                         bs4SidebarMenuSubItem("Cluster Analysis", tabName = "cluster", icon = "angle-double-right"),
-                         bs4SidebarMenuSubItem("Correlation Analysis", tabName = "correlations", icon = "angle-double-right"),
-                         bs4SidebarMenuSubItem("Regularized Regression", tabName = "featureselection", icon = "angle-double-right"),
-                         bs4SidebarMenuSubItem("Random Forest", tabName = "randomforest", icon = "angle-double-right"),
-                         bs4SidebarMenuSubItem("Rank Products", tabName = "rankprod", icon = "angle-double-right"),
-                         bs4SidebarMenuSubItem("Odds Ratio", tabName = "odds", icon = "angle-double-right")
-                         ),
-      bs4SidebarMenuItem("Help", tabName = "help", icon = "question"),
-      # bs4SidebarMenuItem("Tutorials", tabName = "tutorial", icon = "youtube"),
-      bs4SidebarMenuItem("POMA", tabName = "poma", icon = "box"),
-      bs4SidebarMenuItem("License", tabName = "license", icon = "clipboard"),
-      bs4SidebarMenuItem("Code of Conduct", tabName = "conduct", icon = "clipboard-check"),
-      bs4SidebarMenuItem("Contact", tabName = "contact", icon = "user")
+      menuItem("Home", tabName = "home", icon = icon("home")),
+      # bs4SidebarMenuItem("Upload Data", tabName = "inputdata", icon = "upload"),
+      # bs4SidebarMenuItem("Pre-processing", tabName = "preprocessing", icon = "wrench", startExpanded = FALSE,
+      #                    bs4SidebarMenuSubItem("Impute Values", tabName = "impute_vals", icon = "angle-double-right"),
+      #                    bs4SidebarMenuSubItem("Normalization", tabName = "normalization", icon = "angle-double-right"),
+      #                    bs4SidebarMenuSubItem("Outlier Detection", tabName = "outliers", icon = "angle-double-right")
+      #                    ),
+      # bs4SidebarMenuItem("EDA", tabName = "visualization", icon = "search", startExpanded = FALSE,
+      #                    bs4SidebarMenuSubItem("Volcano Plot", tabName = "volcanoPlot", icon = "angle-double-right"),
+      #                    bs4SidebarMenuSubItem("Boxplot", tabName = "boxPlot", icon = "angle-double-right"),
+      #                    bs4SidebarMenuSubItem("Density Plot", tabName = "density", icon = "angle-double-right"),
+      #                    bs4SidebarMenuSubItem("Heatmap", tabName = "HeatMap", icon = "angle-double-right")
+      #                    ),
+      # bs4SidebarMenuItem("Statistical Analysis", tabName = "statistics", icon = "chart-bar", startExpanded = FALSE,
+      #                    bs4SidebarMenuSubItem("Univariate Analysis", tabName = "univariate", icon = "angle-double-right"),
+      #                    bs4SidebarMenuSubItem("Multivariate Analysis", tabName = "multivariate", icon = "angle-double-right"),
+      #                    bs4SidebarMenuSubItem("Cluster Analysis", tabName = "cluster", icon = "angle-double-right"),
+      #                    bs4SidebarMenuSubItem("Correlation Analysis", tabName = "correlations", icon = "angle-double-right"),
+      #                    bs4SidebarMenuSubItem("Regularized Regression", tabName = "featureselection", icon = "angle-double-right"),
+      #                    bs4SidebarMenuSubItem("Random Forest", tabName = "randomforest", icon = "angle-double-right"),
+      #                    bs4SidebarMenuSubItem("Rank Products", tabName = "rankprod", icon = "angle-double-right"),
+      #                    bs4SidebarMenuSubItem("Odds Ratio", tabName = "odds", icon = "angle-double-right")
+      #                    ),
+      # bs4SidebarMenuItem("Help", tabName = "help", icon = "question"),
+      # # bs4SidebarMenuItem("Tutorials", tabName = "tutorial", icon = "youtube"),
+      # bs4SidebarMenuItem("POMA", tabName = "poma", icon = "box"),
+      # bs4SidebarMenuItem("License", tabName = "license", icon = "clipboard"),
+      # bs4SidebarMenuItem("Code of Conduct", tabName = "conduct", icon = "clipboard-check"),
+      menuItem("Contact", tabName = "contact", icon = icon("user"))
       )
     ),
   
   ## CONTROLBAR ----------------------------------------------------------------------
   
-  bs4DashControlbar(
-    skin = "light",
-    title = "Additional Information",
-    
-    bs4DashControlbarMenu(
-      id = "controlbar_menu",
-      status = "primary",
-      side = "right", 
-      vertical = FALSE,
-      
-      bs4DashControlbarItem(
-        tabName = "Active Dataset",
-        active = TRUE,
-        
-        verbatimTextOutput("samples_num"),
-        verbatimTextOutput("groups_num"),
-        verbatimTextOutput("features_num"),
-        verbatimTextOutput("covariates_num")
-      )
-      )
-    ),
+  # controlbar = dashboardControlbar(
+  #   skin = "light",
+  #   # title = "Additional Information",
+  # 
+  #   controlbarMenu(
+  #     # id = "controlbar_menu",
+  #     status = "primary",
+  #     side = "right",
+  #     vertical = FALSE,
+  # 
+  #     controlbarItem(
+  #       # tabName = "Active Dataset",
+  #       active = TRUE,
+  # 
+  #       verbatimTextOutput("samples_num"),
+  #       verbatimTextOutput("groups_num"),
+  #       verbatimTextOutput("features_num"),
+  #       verbatimTextOutput("covariates_num")
+  #     )
+  #     )
+  #   ),
   
   ## BODY ----------------------------------------------------------------------
   
-  bs4DashBody(
+  body = dashboardBody(
     
     use_theme(poma_theme),
       
-    bs4TabItems(
+    tabItems(
       
-      bs4TabItem("home",
-                 source("ui-tab-landing.R", local=TRUE)$value),
-      bs4TabItem("inputdata",
-                 source("ui-tab-inputdata.R", local=TRUE)$value),
-      bs4TabItem("impute_vals",
-                 source("ui-tab-imputevalues.R", local=TRUE)$value),
-      bs4TabItem("normalization",
-                 source("ui-tab-normalization.R", local=TRUE)$value),
-      bs4TabItem("outliers",
-                 source("ui-tab-outliers.R", local=TRUE)$value),
-      bs4TabItem("volcanoPlot",
-                 source("ui-tab-volcano.R", local=TRUE)$value),
-      bs4TabItem("boxPlot",
-                 source("ui-tab-boxplot.R", local=TRUE)$value),
-      bs4TabItem("density",
-                 source("ui-tab-density.R", local=TRUE)$value),
-      bs4TabItem("HeatMap",
-                 source("ui-tab-heatmap.R", local=TRUE)$value),
-      bs4TabItem("univariate",
-                 source("ui-tab-univariate.R", local=TRUE)$value),
-      bs4TabItem("multivariate",
-                 source("ui-tab-multivariate.R", local=TRUE)$value),
-      bs4TabItem("cluster",
-                 source("ui-tab-cluster.R", local=TRUE)$value),
-      bs4TabItem("correlations",
-                 source("ui-tab-correlations.R", local=TRUE)$value),
-      bs4TabItem("featureselection",
-                 source("ui-tab-featureselection.R", local=TRUE)$value),
-      bs4TabItem("randomforest",
-                 source("ui-tab-random_forest.R", local=TRUE)$value),
-      bs4TabItem("rankprod",
-                 source("ui-tab-rankprod.R", local=TRUE)$value),
-      bs4TabItem("odds",
-                 source("ui-tab-odds.R", local=TRUE)$value),
-      bs4TabItem("help",
-                 source("ui-tab-help.R", local=TRUE)$value),
-      # bs4TabItem("tutorial",
-      #            source("ui-tab-tutorial.R", local=TRUE)$value),
-      bs4TabItem("poma",
-                 source("ui-tab-poma.R", local=TRUE)$value),
-      bs4TabItem("license",
-                 source("ui-tab-license.R", local=TRUE)$value),
-      bs4TabItem("conduct",
-                 source("ui-tab-conduct.R", local=TRUE)$value),
-      bs4TabItem("contact",
-                 source("ui-tab-contact.R", local=TRUE)$value)
-      ) # bs4TabItems
-    ), # bs4DashBody
+      tabItem("home",
+              source("ui-tab-landing.R", local=TRUE)$value),
+      tabItem("inputdata",
+              source("ui-tab-inputdata.R", local=TRUE)$value),
+      tabItem("impute_vals",
+              source("ui-tab-imputevalues.R", local=TRUE)$value),
+      tabItem("normalization",
+              source("ui-tab-normalization.R", local=TRUE)$value),
+      tabItem("outliers",
+              source("ui-tab-outliers.R", local=TRUE)$value),
+      tabItem("volcanoPlot",
+              source("ui-tab-volcano.R", local=TRUE)$value),
+      tabItem("boxPlot",
+              source("ui-tab-boxplot.R", local=TRUE)$value),
+      tabItem("density",
+              source("ui-tab-density.R", local=TRUE)$value),
+      tabItem("HeatMap",
+              source("ui-tab-heatmap.R", local=TRUE)$value),
+      tabItem("univariate",
+              source("ui-tab-univariate.R", local=TRUE)$value),
+      tabItem("multivariate",
+              source("ui-tab-multivariate.R", local=TRUE)$value),
+      tabItem("cluster",
+              source("ui-tab-cluster.R", local=TRUE)$value),
+      tabItem("correlations",
+              source("ui-tab-correlations.R", local=TRUE)$value),
+      tabItem("featureselection",
+              source("ui-tab-featureselection.R", local=TRUE)$value),
+      tabItem("randomforest",
+              source("ui-tab-random_forest.R", local=TRUE)$value),
+      tabItem("rankprod",
+              source("ui-tab-rankprod.R", local=TRUE)$value),
+      tabItem("odds",
+              source("ui-tab-odds.R", local=TRUE)$value),
+      tabItem("help",
+              source("ui-tab-help.R", local=TRUE)$value),
+      tabItem("poma",
+              source("ui-tab-poma.R", local=TRUE)$value),
+      tabItem("license",
+              source("ui-tab-license.R", local=TRUE)$value),
+      tabItem("conduct",
+              source("ui-tab-conduct.R", local=TRUE)$value),
+      tabItem("contact",
+              source("ui-tab-contact.R", local=TRUE)$value)
+      ) # tabItems
+    )#, # bs4DashBody
   
   ## FOOTER ----------------------------------------------------------------------
   
-  bs4DashFooter(
-    
-    fluidRow(
-      column(
-        width = 12,
-        align = "center",
-        a(href = "https://pcastellanoescuder.github.io", HTML("<b>Pol Castellano Escuder</b>")), ", ",
-        a(href = "http://www.nutrimetabolomics.com/members/raul-gonzalez-dominguez", "Raúl González Domínguez"), ", ",
-        a(href = "https://sites.google.com/view/estbioinfo/home?authuser=0", "Francesc Carmona Pontaque"), ", ",
-        a(href = "http://www.nutrimetabolomics.com/team/cristina", "Cristina Andrés Lacueva"), " and ",
-        a(href = "https://webgrec.ub.edu/webpages/000011/cat/asanchez.ub.edu.html", "Alex Sánchez Pla"), 
-        br(),
-        "Statistics and Bioinformatics Research Group", "and",
-        "Biomarkers and Nutritional & Food Metabolomics Research Group", "from",
-        br(),
-        "University of Barcelona",
-        br(),
-        "Copyright (C) 2021, code licensed under GPL-3.0",
-        br(),
-        "Code available on Github:", a(href = "https://github.com/pcastellanoescuder/POMAShiny", "https://github.com/pcastellanoescuder/POMAShiny")
-        )
-      ),
-    right_text = NULL
-    )
+  # dashboardFooter(
+  #   
+  #   fluidRow(
+  #     column(
+  #       width = 12,
+  #       align = "center",
+  #       a(href = "https://pcastellanoescuder.github.io", HTML("<b>Pol Castellano Escuder</b>")), ", ",
+  #       a(href = "http://www.nutrimetabolomics.com/members/raul-gonzalez-dominguez", "Raúl González Domínguez"), ", ",
+  #       a(href = "https://sites.google.com/view/estbioinfo/home?authuser=0", "Francesc Carmona Pontaque"), ", ",
+  #       a(href = "http://www.nutrimetabolomics.com/team/cristina", "Cristina Andrés Lacueva"), " and ",
+  #       a(href = "https://webgrec.ub.edu/webpages/000011/cat/asanchez.ub.edu.html", "Alex Sánchez Pla"), 
+  #       br(),
+  #       "Statistics and Bioinformatics Research Group", "and",
+  #       "Biomarkers and Nutritional & Food Metabolomics Research Group", "from",
+  #       br(),
+  #       "University of Barcelona",
+  #       br(),
+  #       "Copyright (C) 2021, code licensed under GPL-3.0",
+  #       br(),
+  #       "Code available on Github:", a(href = "https://github.com/pcastellanoescuder/POMAShiny", "https://github.com/pcastellanoescuder/POMAShiny")
+  #       )
+  #     ),
+  #   right_text = NULL
+  #   )
   ) # bs4DashPage
 
